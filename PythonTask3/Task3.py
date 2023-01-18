0886f485dc7684d1e8a2b352e0c505f499cecd1e
Portey Vasiliy <122995978+VasiliyPortey@users.noreply.github.com>
2023-01-18 19:40:17 +0300

    Add files via upload
    тут я сделал две задачки, одна из них была в файле "3 ПР", другая в отдельном файле с задачками для третьей практической работы, выбрал вариант 1

---
+++ PythonTask3/Task3.py
@@ -0,0 +1,11 @@
+#з файла "пр3 задание" вариант 1:
+a = int(input('Введите целое число a: '))
+b = int(input('Введите целое число b: '))
+print('a =', a, 'b =', b)
+if a<b:
+    x = a+b
+elif a>b:
+    x = a-b
+else:
+    x=1
+print('x =', x)
